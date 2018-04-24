87632fc8e2eb8cf39e7741ded4043fde97271d34
vinaykhedekar21 <33795792+vinaykhedekar21@users.noreply.github.com>
2018-04-23 17:35:54 -0700

    Ember framework startup application

---
+++ app/routes/scientists.js
@@ -0,0 +1,7 @@
+import Route from '@ember/routing/route';
+
+export default Route.extend({
+  model(){
+    return['Vinay Khedekar','Arya bhatt','Abdul Kalam','Albert Estine'];                                
+  }
+});
